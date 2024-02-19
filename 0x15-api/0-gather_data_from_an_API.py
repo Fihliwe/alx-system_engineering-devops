633f60cd9643970dae442e7e810e5da236ec141e
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2024-02-19 13:01:50 +0000

    mod file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <user_id>".format(sys.argv[0]))
-        sys.exit(1)
-        
-        
     url = "https://jsonplaceholder.typicode.com/"
-    user_id = sys.argv[1]
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
